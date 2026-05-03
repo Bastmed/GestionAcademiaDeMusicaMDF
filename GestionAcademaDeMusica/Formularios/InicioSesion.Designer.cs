2185671ba15ca31c6fba3927c7a0248b704a1441
Angel237-alt <sevsmg2@gmail.com>
2026-05-03 19:11:01 -0400

    Inicio de sesion con diseño

--- GestionAcademaDeMusica/Formularios/InicioSesion.Designer.cs
+++ GestionAcademaDeMusica/Formularios/InicioSesion.Designer.cs
@@ -34,71 +34,110 @@
             this.txtNombreUsuario = new System.Windows.Forms.TextBox();
             this.txtContraseñaUsuario = new System.Windows.Forms.TextBox();
             this.btnIniciarSesion = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
             // lblInicioSesion
             // 
             this.lblInicioSesion.AutoSize = true;
-            this.lblInicioSesion.Location = new System.Drawing.Point(366, 38);
+            this.lblInicioSesion.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInicioSesion.ForeColor = System.Drawing.Color.Goldenrod;
+            this.lblInicioSesion.Location = new System.Drawing.Point(498, 190);
+            this.lblInicioSesion.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblInicioSesion.Name = "lblInicioSesion";
-            this.lblInicioSesion.Size = new System.Drawing.Size(80, 13);
+            this.lblInicioSesion.Size = new System.Drawing.Size(234, 41);
             this.lblInicioSesion.TabIndex = 0;
             this.lblInicioSesion.Text = "Inicio de sesión";
             // 
             // lblNombreUsuario
             // 
             this.lblNombreUsuario.AutoSize = true;
-            this.lblNombreUsuario.Location = new System.Drawing.Point(272, 107);
+            this.lblNombreUsuario.Location = new System.Drawing.Point(439, 319);
+            this.lblNombreUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombreUsuario.Name = "lblNombreUsuario";
-            this.lblNombreUsuario.Size = new System.Drawing.Size(47, 13);
+            this.lblNombreUsuario.Size = new System.Drawing.Size(77, 23);
             this.lblNombreUsuario.TabIndex = 1;
             this.lblNombreUsuario.Text = "Nombre:";
             // 
             // lblContraseñaUsuario
             // 
             this.lblContraseñaUsuario.AutoSize = true;
-            this.lblContraseñaUsuario.Location = new System.Drawing.Point(272, 185);
+            this.lblContraseñaUsuario.Location = new System.Drawing.Point(439, 393);
+            this.lblContraseñaUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblContraseñaUsuario.Name = "lblContraseñaUsuario";
-            this.lblContraseñaUsuario.Size = new System.Drawing.Size(64, 13);
+            this.lblContraseñaUsuario.Size = new System.Drawing.Size(101, 23);
             this.lblContraseñaUsuario.TabIndex = 2;
             this.lblContraseñaUsuario.Text = "Contraseña:";
             // 
             // txtNombreUsuario
             // 
-            this.txtNombreUsuario.Location = new System.Drawing.Point(422, 107);
+            this.txtNombreUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNombreUsuario.Location = new System.Drawing.Point(615, 319);
+            this.txtNombreUsuario.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.txtNombreUsuario.Name = "txtNombreUsuario";
-            this.txtNombreUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtNombreUsuario.Size = new System.Drawing.Size(148, 30);
             this.txtNombreUsuario.TabIndex = 3;
             // 
             // txtContraseñaUsuario
             // 
-            this.txtContraseñaUsuario.Location = new System.Drawing.Point(422, 178);
+            this.txtContraseñaUsuario.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtContraseñaUsuario.Location = new System.Drawing.Point(615, 391);
+            this.txtContraseñaUsuario.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.txtContraseñaUsuario.Name = "txtContraseñaUsuario";
-            this.txtContraseñaUsuario.Size = new System.Drawing.Size(100, 20);
+            this.txtContraseñaUsuario.Size = new System.Drawing.Size(148, 30);
             this.txtContraseñaUsuario.TabIndex = 4;
             // 
             // btnIniciarSesion
             // 
-            this.btnIniciarSesion.Location = new System.Drawing.Point(369, 265);
+            this.btnIniciarSesion.BackColor = System.Drawing.Color.Indigo;
+            this.btnIniciarSesion.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnIniciarSesion.FlatAppearance.BorderSize = 0;
+            this.btnIniciarSesion.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIniciarSesion.ForeColor = System.Drawing.Color.White;
+            this.btnIniciarSesion.Location = new System.Drawing.Point(534, 503);
+            this.btnIniciarSesion.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.btnIniciarSesion.Name = "btnIniciarSesion";
-            this.btnIniciarSesion.Size = new System.Drawing.Size(103, 23);
+            this.btnIniciarSesion.Size = new System.Drawing.Size(154, 40);
             this.btnIniciarSesion.TabIndex = 5;
             this.btnIniciarSesion.Text = "Iniciar sesión";
-            this.btnIniciarSesion.UseVisualStyleBackColor = true;
-
+            this.btnIniciarSesion.UseVisualStyleBackColor = false;
             this.btnIniciarSesion.Click += new System.EventHandler(this.btnIniciarSesion_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Indigo;
+            this.panel1.Location = new System.Drawing.Point(-1, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(164, 800);
+            this.panel1.TabIndex = 6;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.Location = new System.Drawing.Point(1002, -3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 800);
+            this.panel2.TabIndex = 7;
             // 
             // InicioSesion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = global::GestionAcademaDeMusica.Properties.Resources.images_removebg_preview;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(1200, 796);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.btnIniciarSesion);
             this.Controls.Add(this.txtContraseñaUsuario);
             this.Controls.Add(this.txtNombreUsuario);
             this.Controls.Add(this.lblContraseñaUsuario);
             this.Controls.Add(this.lblNombreUsuario);
             this.Controls.Add(this.lblInicioSesion);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.Name = "InicioSesion";
             this.Text = "InicioSesion";
             this.ResumeLayout(false);
@@ -114,5 +153,7 @@
         private System.Windows.Forms.TextBox txtNombreUsuario;
         private System.Windows.Forms.TextBox txtContraseñaUsuario;
         private System.Windows.Forms.Button btnIniciarSesion;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
     }
 }
